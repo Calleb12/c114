2e740783350931273cf52335ed0ddcd69b8756fc
Calleb12 <116227059+Calleb12@users.noreply.github.com>
2023-09-20 18:20:16 -0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,6 @@
+a = 33
+b = 33
+if b> a:
+ print("b maior que a")
+elif b == a:
+ print("a e b são iguais")
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+ print(fname)
+
+my_function("emilly")
+my_function("tobias")
+my_function("lucas")
